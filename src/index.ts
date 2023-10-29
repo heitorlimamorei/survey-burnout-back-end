a7f420be215ca02bbf178bb3a3d9ff0f8b91127d
Heitor Lima Moreira <61924603+heitorlimamorei@users.noreply.github.com>
2023-10-29 17:50:42 -0300

    feat: add cors into the app

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import dotenv from  'dotenv';
 import SurveyRouter from "./routers/survey.router";
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/survey', SurveyRouter);
